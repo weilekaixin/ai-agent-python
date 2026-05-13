19bbe8d06716791233d9eebc1295d4d70aea863c
zhangyunlong <122527725@qq.com>
2026-05-13 23:28:29 +0800

    fix：注册工具

---
+++ src/ai_agent/modules/tool/tools.py
@@ -0,0 +1,23 @@
+from datetime import datetime
+
+from langchain_core.tools import tool
+
+
+@tool
+def get_current_time() -> str:
+    """获取实时时间"""
+    return datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+
+
+@tool
+def calculator(a: float, b: float, op: str) -> str:
+    """四则运算"""
+    if op == '+':
+        return str(a + b)
+    elif op == '-':
+        return str(a - b)
+    elif op == '*':
+        return str(a * b)
+    elif op == '/':
+        return str(a / b)
+    return "error"
--- tests/main.py
+++ tests/main.py
@@ -1,13 +1,17 @@
 import asyncio
 
 from ai_agent.modules.llm.factory import get_llm
+from ai_agent.modules.tool.tools import get_current_time, calculator
 
 
 async def agent():
+    # 获取大模型
     llm = get_llm()
-    print(llm)
-    result = await llm.ainvoke([{"role": "user", "content": "你好？"}])
-    print(result.content)
+    # 注册工具
+    llm_with_tools = llm.bind_tools([get_current_time, calculator])
+    print(llm, llm_with_tools)
+    result = await llm_with_tools.ainvoke([{"role": "user", "content": "现在几点了？"}])
+    print(result)
 
 
 if __name__ == '__main__':
